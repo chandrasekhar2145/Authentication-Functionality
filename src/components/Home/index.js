b169d635b33753606721595b847902a0c8db206d
chandu <cs8074970147@gmail.com>
2024-03-02 07:12:19 +0000

    CODING PRACTICE 29

---
+++ src/components/Home/index.js
@@ -0,0 +1,13 @@
+// Write your JS code here
+import Header from '../Header'
+import LogoutButton from '../LogoutButton'
+
+const Home = () => {
+  ;<>
+    <Header />
+    <h1 className="home">Home</h1>
+    <LogoutButton />
+  </>
+}
+
+export default Home
